947a9ed6047963a4205360ac0c94c2149f6abba3
R3ndrex <yacholohchtoli@gmail.com>
2025-02-05 19:05:16 +0200

    Add subscribe on changing units value

--- src/index.js
+++ src/index.js
@@ -2,31 +2,52 @@ import "./style.css";
 import getConditionImage from "./asets/getConditionImage.js";
 import WeatherBuilder from "./asets/weatherBuilder.js";
 import WeatherWidget from "./asets/weatherWidget.js";
-const API = "HDDUWHXU3LRBLBP2TD8WGNNJV";
+import mediator from "./asets/mediator.js";
+
 const input = document.querySelector("input");
 const Seearchbutton = document.querySelector(".search-button");
 let units = "metric";
 const unitChanger = document.querySelector("#units");
+const locationButton = document.querySelector(".location-button");
 const main = document.querySelector("main");
 
-const weatherWidget = new WeatherBuilder(main, getConditionImage);
-const weatherBuilder = new WeatherWidget(API);
+const weatherBuilder = new WeatherBuilder(main, getConditionImage);
+const weatherWidget = new WeatherWidget("HDDUWHXU3LRBLBP2TD8WGNNJV");
+weatherBuilder.setMediator(mediator);
+mediator.subscribe("ChangedUnits", () => {
+    weatherWidget
+        .getWeather(`/${input.value}?iconSet=icons2&unitGroup=${units}`)
+        .then((json) => weatherBuilder.init(json));
+});
 
 Seearchbutton.addEventListener("click", () => {
     input.setCustomValidity("");
-    weatherBuilder
+    weatherWidget
         .getWeather(`/${input.value}?iconSet=icons2&unitGroup=${units}`)
-        .then((json) => {
-            weatherWidget.init(json);
-        })
-        .catch((error) => {
-            console.error(error);
-            input.setCustomValidity("Invalid input. Please enter real city.");
-            input.reportValidity();
+        .then((json) => weatherBuilder.init(json))
+        .catch((error) => CheckValidity(input, error));
+});
+
+locationButton.addEventListener("click", () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            console.log(position["coords"]["longitude"]);
+            console.log(position["coords"]["latitude"]);
         });
+    }
 });
 
 unitChanger.addEventListener("change", (e) => {
     units = e.target.value;
-    weatherWidget.changeUnits(e.target.value);
+    weatherBuilder.units = e.target.value;
 });
+
+function CheckValidity(element, error) {
+    if (element.validity.valueMissing) {
+        input.setCustomValidity("Please enter city name.");
+    } else {
+        console.error(error);
+        input.setCustomValidity("Please enter correct city.");
+    }
+    input.reportValidity();
+}
