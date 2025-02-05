d1bb26e707ffd266a50279469b8d20090c301d49
R3ndrex <yacholohchtoli@gmail.com>
2025-02-05 09:24:51 +0200

    add repoty validation on error, remove comments

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
 import "./style.css";
 import getConditionImage from "./asets/setConditionImage.js";
+
 const weatherConditions = document.querySelector(".weather-info");
 const API = "HDDUWHXU3LRBLBP2TD8WGNNJV";
-const address = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline`; // + [location]/[date1]/[date2]?key=YOUR_API_KEY
+const address = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline`;
 const input = document.querySelector("input");
 const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
-    if (input.value.trim() === "") {
-        return;
-    }
-    GetWeather(input.value).then((response) => BuildInfoBlock(response));
+    input.setCustomValidity("");
+    GetWeather(input.value)
+        .then((response) => BuildInfoBlock(response))
+        .catch((reject) => {
+            console.error(reject.message);
+            input.setCustomValidity("Invalid input. Please enter real city.");
+            input.reportValidity();
+        });
 });
 
 async function GetWeather(city) {
-    try {
-        const response = await fetch(
-            `${address}/${city}?iconSet=icons2&key=${API}`
-        );
-        const json = await response.json();
-        return json;
-    } catch (error) {
-        console.error(error.message);
-    }
+    const response = await fetch(
+        `${address}/${city}?iconSet=icons2&key=${API}`
+    );
+    const json = await response.json();
+    return json;
 }
 
 function BuildInfoBlock(json) {
