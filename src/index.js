4617859676eb484c2bfc54c8c7665faca389479a
R3ndrex <yacholohchtoli@gmail.com>
2025-02-05 12:10:55 +0200

    add units for temperature, display condition, temperature, description,feelslike temperature

--- src/index.js
+++ src/index.js
@@ -6,21 +6,26 @@ const API = "HDDUWHXU3LRBLBP2TD8WGNNJV";
 const address = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline`;
 const input = document.querySelector("input");
 const button = document.querySelector("button");
+let units = "metric";
+const unitChanger = document.querySelector("#units");
 
 button.addEventListener("click", () => {
     input.setCustomValidity("");
     GetWeather(input.value)
         .then((response) => BuildInfoBlock(response))
-        .catch((reject) => {
-            console.error(reject.message);
+        .catch((error) => {
+            console.error(error);
             input.setCustomValidity("Invalid input. Please enter real city.");
             input.reportValidity();
         });
 });
+unitChanger.addEventListener("change", (e) => {
+    units = e.target.value;
+});
 
 async function GetWeather(city) {
     const response = await fetch(
-        `${address}/${city}?iconSet=icons2&key=${API}`
+        `${address}/${city}?iconSet=icons2&unitGroup=${units}&key=${API}`
     );
     const json = await response.json();
     return json;
@@ -29,9 +34,32 @@ async function GetWeather(city) {
 function BuildInfoBlock(json) {
     weatherConditions.innerHTML = "";
     console.log(json);
-    weatherConditions.textContent = json["currentConditions"]["conditions"];
+    const condition = document.createElement("div");
+    condition.textContent = json["currentConditions"]["conditions"];
+    const descriptions = document.createElement("div");
+    descriptions.textContent = json["description"];
     const icon = document.createElement("img");
     icon.src = getConditionImage(json["currentConditions"]["icon"]);
     icon.alt = json["currentConditions"]["icon"];
-    weatherConditions.appendChild(icon);
+    const temperature = document.createElement("h2");
+    temperature.textContent = json["currentConditions"]["temp"];
+    const h3 = document.createElement("h3");
+    h3.textContent =
+        json["address"][0].toUpperCase() + json["address"].slice(1);
+    if (units === "metric") {
+        temperature.textContent += "℃";
+    } else {
+        temperature.textContent += "℉";
+    }
+    const feelsLike = document.createElement("div");
+    feelsLike.textContent =
+        "feels like" + json["currentConditions"]["feelslike"];
+    const row = document.createElement("div");
+    row.appendChild(temperature);
+    row.appendChild(icon);
+    weatherConditions.appendChild(h3);
+    weatherConditions.appendChild(row);
+    weatherConditions.appendChild(feelsLike);
+    weatherConditions.appendChild(condition);
+    weatherConditions.appendChild(descriptions);
 }
