2b5fb16a0c8caa1582856e67b38989b757ebacf2
R3ndrex <yacholohchtoli@gmail.com>
2025-02-05 09:25:55 +0200

    Add return null if not in conditions object, add all images to conditions

--- src/asets/setConditionImage.js
+++ src/asets/setConditionImage.js
@@ -1,4 +1,4 @@
-import ClodyImage from "../images/cloudy.png";
+import CloudyImage from "../images/cloudy.png";
 import ClearNightImage from "../images/clear-night.png";
 import ClearDayImage from "../images/clear-day.png";
 import FogImage from "../images/fog.png";
@@ -12,9 +12,13 @@ import ShowersDayImage from "../images/showers-day.png";
 import ThunderRainImage from "../images/thunder-rain.png";
 import SnowShowersDayImage from "../images/snow-showers-day.png";
 import SnowShowersNightImage from "../images/snow-showers-night.png";
+import ThunderShowersNightImage from "../images/thunder-showers-night.png";
+import ThunderShowersDayImage from "../images/thunder-showers-day.png";
+import RainSnowShowersDayImage from "../images/rain-snow-showers-day.png";
+import RainSnowShowersNightImage from "../images/rain-snow-showers-night.png";
 
 const conditions = {
-    cloudy: ClodyImage,
+    cloudy: CloudyImage,
     "clear-night": ClearNightImage,
     "clear-day": ClearDayImage,
     fog: FogImage,
@@ -28,13 +32,17 @@ const conditions = {
     "thunder-rain": ThunderRainImage,
     "snow-showers-day": SnowShowersDayImage,
     "snow-showers-night": SnowShowersNightImage,
-    "thunder-showers-night": ThunderRainImage,
-    "thunder-showers-day": ThunderRainImage,
+    "thunder-showers-night": ThunderShowersNightImage,
+    "thunder-showers-day": ThunderShowersDayImage,
+    // those maybe not working cus, i didnt see those conditions in documentations, but there was images for it
+    "rain-snow-showers-day": RainSnowShowersDayImage,
+    "rain-snow-showers-night": RainSnowShowersNightImage,
 };
-// what does rain-snow-showers-day and night are for? idk
 function getConditionImage(condition) {
     if (condition in conditions) {
         return conditions[condition];
+    } else {
+        return null;
     }
 }
 export default getConditionImage;
