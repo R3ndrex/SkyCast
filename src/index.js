36743a7ca614c78ae97cbd5ec56cead30a6a5afc
R3ndrex <yacholohchtoli@gmail.com>
2025-02-05 17:30:28 +0200

    Import classes, divide functionality into other files

--- src/index.js
+++ src/index.js
@@ -1,75 +1,32 @@
 import "./style.css";
-import getConditionImage from "./asets/setConditionImage.js";
-
+import getConditionImage from "./asets/getConditionImage.js";
+import WeatherBuilder from "./asets/weatherBuilder.js";
+import WeatherWidget from "./asets/weatherWidget.js";
 const API = "HDDUWHXU3LRBLBP2TD8WGNNJV";
-const address = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline`;
 const input = document.querySelector("input");
-const button = document.querySelector("button");
+const Seearchbutton = document.querySelector(".search-button");
 let units = "metric";
 const unitChanger = document.querySelector("#units");
+const main = document.querySelector("main");
 
-button.addEventListener("click", () => {
+const weatherWidget = new WeatherBuilder(main, getConditionImage);
+const weatherBuilder = new WeatherWidget(API);
+
+Seearchbutton.addEventListener("click", () => {
     input.setCustomValidity("");
-    GetWeather(input.value)
-        .then((response) => BuildInfoBlock(response))
+    weatherBuilder
+        .getWeather(`/${input.value}?iconSet=icons2&unitGroup=${units}`)
+        .then((json) => {
+            weatherWidget.init(json);
+        })
         .catch((error) => {
             console.error(error);
             input.setCustomValidity("Invalid input. Please enter real city.");
             input.reportValidity();
         });
 });
+
 unitChanger.addEventListener("change", (e) => {
     units = e.target.value;
+    weatherWidget.changeUnits(e.target.value);
 });
-
-async function GetWeather(city) {
-    const response = await fetch(
-        `${address}/${city}?iconSet=icons2&unitGroup=${units}&key=${API}`
-    );
-    const json = await response.json();
-    return json;
-}
-
-function BuildInfoBlock(json) {
-    try {
-        document
-            .querySelector("main")
-            .removeChild(document.querySelector(".weather-info"));
-    } catch {}
-    const weatherConditions = document.createElement("div");
-    weatherConditions.classList.add("weather-info");
-    weatherConditions.innerHTML = "";
-    const condition = document.createElement("div");
-    condition.textContent = json["currentConditions"]["conditions"];
-    const descriptions = document.createElement("div");
-    descriptions.textContent = json["description"];
-    const icon = document.createElement("img");
-    icon.src = getConditionImage(json["currentConditions"]["icon"]);
-    icon.alt = json["currentConditions"]["icon"];
-    const temperature = document.createElement("h2");
-    temperature.textContent = json["currentConditions"]["temp"];
-    const h3 = document.createElement("h3");
-
-    h3.textContent =
-        json["address"][0].toUpperCase() + json["address"].slice(1);
-    const feelsLike = document.createElement("div");
-    feelsLike.textContent =
-        "feels like: " + json["currentConditions"]["feelslike"];
-    if (units === "metric") {
-        temperature.textContent += "℃";
-        feelsLike.textContent += "℃";
-    } else {
-        temperature.textContent += "℉";
-        feelsLike.textContent += "℉";
-    }
-
-    const row = document.createElement("div");
-    row.appendChild(temperature);
-    row.appendChild(icon);
-    weatherConditions.appendChild(h3);
-    weatherConditions.appendChild(row);
-    weatherConditions.appendChild(feelsLike);
-    weatherConditions.appendChild(condition);
-    weatherConditions.appendChild(descriptions);
-    document.querySelector("main").appendChild(weatherConditions);
-}
